da88b762e973d9b34d9f6f63c0a2162f83b163ab
epicplayer2611 <epicplayer2611@gmail.com>
2026-05-16 13:33:26 -0500

    Update ENPSend.lua

--- CustomAPIs/EpicNetworkProtocol/ENPSend.lua
+++ CustomAPIs/EpicNetworkProtocol/ENPSend.lua
@@ -1,1 +1,13 @@
 --EpicNetworkProtocol Sender
+local function loadData(path)
+    if not fs.exists(path) then
+        return {}   -- safe default
+    end
+    local f = fs.open(path, "r")
+    local raw = f.readAll()
+    f.close()
+    return textutils.unserialise(raw) or {}
+end
+
+local protocolData = loadData("ENPData.dat")
+
